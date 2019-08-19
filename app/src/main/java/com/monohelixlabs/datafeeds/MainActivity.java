06221676efe1d0696bfd204126f7502f85ab6503
rirozizo <rawadzahreddine32@gmail.com>
2019-08-20 00:44:44 +0300

    Updated Gradle, removed useless TODO lines, fixed some margins

--- app/src/main/java/com/monohelixlabs/datafeeds/MainActivity.java
+++ app/src/main/java/com/monohelixlabs/datafeeds/MainActivity.java
@@ -73,10 +73,8 @@ public class MainActivity extends ListActivity {
 
 
             } catch (MalformedURLException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             } catch (IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
             return result;
